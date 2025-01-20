1d22d0ab4c9ffcc11a913b0cf01cf2738a976ab8
Construct Admin <admin@proversity.org>
2025-01-20 10:45:29 +0200

    Update construct_lo_generator.py

--- construct_lo_generator.py
+++ construct_lo_generator.py
@@ -203,7 +203,7 @@ PHASES = {
 # Page configuration
 PAGE_CONFIG = {
     "page_title": "Construct LO Generator",
-    "page_icon": "️app_images/construct.webp",
+    "page_icon": "️app_images\construct.webp",
     "layout": "centered",
     "initial_sidebar_state": "expanded"
 }
