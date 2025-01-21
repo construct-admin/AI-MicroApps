948371a68230f2fb0838f3843c8e6bcc10bb023b
Construct Admin <admin@proversity.org>
2025-01-21 10:34:15 +0200

    Update quiz_question_generator.py

--- quiz_question_generator.py
+++ quiz_question_generator.py
@@ -16,7 +16,106 @@ Output:
 
 Constraints:
 - Ensure that distractors are viable and that the question is not too easy to answer.
-- Emphasize higher-level thinking."""
+- Emphasize higher-level thinking.
+
+Apply the formatting as seen in the examples below. Indicate the correct answer by using an asterisk.
+
+Example output for each output format.
+
+Selection: General Quiz Feedback
+Which of the following is the weakest scatterer of conducting electrons?
+
+A: Surface of the material.
+
+B: Impurities in the material.
+
+*C: Isotopes of the material.
+
+D: Vibrating atoms within the material.
+
+General Feedback: Isotopes have the least effect on electron scattering because they maintain the chemical properties of the original atoms, causing minimal disruption to the electron flow. This makes them the weakest scatterers among the options presented.
+
+End of example for General Quiz Feedback
+
+Selection: Answer-Option Level Quiz Feedback
+Which of the following is the weakest scatterer of conducting electrons?
+
+A: Surface of the material.
+
+Feedback: Sorry, that is incorrect. The surface of a material can significantly scatter conducting electrons due to the abrupt change in the material's structure and the presence of surface states or defects. This is especially the case for ultra-small samples such as nanowires. However, Isotope atoms are chemically identical to the majority of atoms in the material and thus behave similarly in terms of interacting with electrons.
+
+*B: Isotopes of the material.
+
+Feedback: Correct! Isotope atoms are chemically identical to the majority of atoms in the material and thus behave similarly in terms of interacting with electrons. The slight difference in mass between isotopes typically has a minimal effect on electron scattering compared to other factors.
+
+C: Impurities in the material.
+
+Feedback: Sorry, that is incorrect. Impurities in a material are strong scatterers of conducting electrons. They introduce different potentials and disrupt the periodic lattice, leading to significant electron scattering. However, Isotope atoms are chemically identical to the majority of atoms in the material and thus behave similarly in terms of interacting with electrons.
+
+D: Vibrating atoms within the material.
+
+Feedback: Sorry, that is incorrect. Vibrating atoms, which are associated with lattice vibrations or phonons, can be significant scatterers of conducting electrons, especially at higher temperatures. However, Isotope atoms are chemically identical to the majority of atoms in the material and thus behave similarly in terms of interacting with electrons.
+
+End of example for Answer-Option Level Quiz Feedback
+
+Selection: Coursera Ungraded Quiz
+Which of the following is the weakest scatterer of conducting electrons?
+
+A: Surface of the material.
+
+Feedback: Sorry, that is incorrect. The surface of a material can significantly scatter conducting electrons due to the abrupt change in the material's structure and the presence of surface states or defects. This is especially the case for ultra-small samples such as nanowires. However, Isotope atoms are chemically identical to the majority of atoms in the material and thus behave similarly in terms of interacting with electrons.
+
+*B: Isotopes of the material.
+
+Feedback: Correct! Isotope atoms are chemically identical to the majority of atoms in the material and thus behave similarly in terms of interacting with electrons. The slight difference in mass between isotopes typically has a minimal effect on electron scattering compared to other factors.
+
+C: Impurities in the material.
+
+Feedback: Sorry, that is incorrect. Impurities in a material are strong scatterers of conducting electrons. They introduce different potentials and disrupt the periodic lattice, leading to significant electron scattering. However, Isotope atoms are chemically identical to the majority of atoms in the material and thus behave similarly in terms of interacting with electrons.
+
+D: Vibrating atoms within the material.
+
+Feedback: Sorry, that is incorrect. Vibrating atoms, which are associated with lattice vibrations or phonons, can be significant scatterers of conducting electrons, especially at higher temperatures. However, Isotope atoms are chemically identical to the majority of atoms in the material and thus behave similarly in terms of interacting with electrons.
+
+End of example for Coursera Ungraded Quiz
+
+Selection: Coursera Graded Quiz
+What is the threshold diameter below which the electrical conduction of a metal nanowire can become worse than that of the bulk?
+
+A: The atomic distance of the metal.
+Feedback: To learn more about the relationship between the diameter of metal nanowires and their electrical conduction properties, review “Resource Placeholder.”
+
+*B: The electron mean free path in the metal.
+Feedback: Correct, the electron mean free path in the metal is the threshold diameter.
+
+C: The electron de Broglie wavelength in the metal.
+Feedback: To learn more about the relationship between the diameter of metal nanowires and their electrical conduction properties, review “Resource Placeholder.”
+
+D: The mean impurity distance in the metal.
+Feedback: To learn more about the relationship between the diameter of metal nanowires and their electrical conduction properties, review “Resource Placeholder.”
+
+End of example for Coursera Graded Quiz
+
+Selection: H5P Textual Upload Feature
+Who founded the Roman city of Barcino, which later became Barcelona?
+
+The Greeks
+*The Romans:::Barcelona was originally founded as a Roman colony named Barcino around the end of the 1st century BC.
+The Visigoths
+The Carthaginians
+
+End of example for H5P Textual Upload Feature
+
+Selection: Open edX OLX Quiz
+>>Add the question text, or prompt, here. This text is required||You can add an optional tip or note related to the prompt like this. <<
+( ) an incorrect answer {{You can specify optional feedback like this, which appears after this answer is submitted.}}
+(x) the correct answer
+( ) an incorrect answer {{You can specify optional feedback for none, a subset, or all of the answers.}}
+||You can add an optional hint like this. Problems that have a hint include a hint button, and this text appears the first time learners select the button.||
+||If you add more than one hint, a different hint appears each time learners select the hint button.||
+
+End of example for Open edX OLX Quiz
+"""
 
 # Helper functions for dynamic conditions
 def get_question_level_conditions():
@@ -33,89 +132,12 @@ def get_question_level_conditions():
 
 def get_output_format_conditions():
     return [
-        {"condition": {"output_format": "General Quiz Feedback"}, "prompt": """Apply the formatting as seen in the example below. Indicate the correct answer by using an asterisk.
-
-Which of the following is the weakest scatterer of conducting electrons?
-
-A: Surface of the material.
-
-B: Impurities in the material.
-
-*C: Isotopes of the material.
-
-D: Vibrating atoms within the material.
-
-General Feedback: Isotopes have the least effect on electron scattering because they maintain the chemical properties of the original atoms, causing minimal disruption to the electron flow. This makes them the weakest scatterers among the options presented."""},
-        {"condition": {"output_format": "Answer-Option Level Quiz Feedback"}, "prompt": """Apply the formatting requirements as seen in the example below. Indicate the correct answer by using an asterisk.
-
-Which of the following is the weakest scatterer of conducting electrons?
-
-A: Surface of the material.
-
-Feedback: Sorry, that is incorrect. The surface of a material can significantly scatter conducting electrons due to the abrupt change in the material's structure and the presence of surface states or defects. This is especially the case for ultra-small samples such as nanowires. However, Isotope atoms are chemically identical to the majority of atoms in the material and thus behave similarly in terms of interacting with electrons.
-
-*B: Isotopes of the material.
-
-Feedback: Correct! Isotope atoms are chemically identical to the majority of atoms in the material and thus behave similarly in terms of interacting with electrons. The slight difference in mass between isotopes typically has a minimal effect on electron scattering compared to other factors.
-
-C: Impurities in the material.
-
-Feedback: Sorry, that is incorrect. Impurities in a material are strong scatterers of conducting electrons. They introduce different potentials and disrupt the periodic lattice, leading to significant electron scattering. However, Isotope atoms are chemically identical to the majority of atoms in the material and thus behave similarly in terms of interacting with electrons.
-
-D: Vibrating atoms within the material.
-
-Feedback: Sorry, that is incorrect. Vibrating atoms, which are associated with lattice vibrations or phonons, can be significant scatterers of conducting electrons, especially at higher temperatures. However, Isotope atoms are chemically identical to the majority of atoms in the material and thus behave similarly in terms of interacting with electrons."""},
-        {"condition": {"output_format": "Coursera Ungraded Quiz"}, "prompt": """Apply the formatting as seen in the example below. Indicate the correct answer by using an asterisk.
-
-Which of the following is the weakest scatterer of conducting electrons?
-
-A: Surface of the material.
-
-Feedback: Sorry, that is incorrect. The surface of a material can significantly scatter conducting electrons due to the abrupt change in the material's structure and the presence of surface states or defects. This is especially the case for ultra-small samples such as nanowires. However, Isotope atoms are chemically identical to the majority of atoms in the material and thus behave similarly in terms of interacting with electrons.
-
-*B: Isotopes of the material.
-
-Feedback: Correct! Isotope atoms are chemically identical to the majority of atoms in the material and thus behave similarly in terms of interacting with electrons. The slight difference in mass between isotopes typically has a minimal effect on electron scattering compared to other factors.
-
-C: Impurities in the material.
-
-Feedback: Sorry, that is incorrect. Impurities in a material are strong scatterers of conducting electrons. They introduce different potentials and disrupt the periodic lattice, leading to significant electron scattering. However, Isotope atoms are chemically identical to the majority of atoms in the material and thus behave similarly in terms of interacting with electrons.
-
-D: Vibrating atoms within the material.
-
-Feedback: Sorry, that is incorrect. Vibrating atoms, which are associated with lattice vibrations or phonons, can be significant scatterers of conducting electrons, especially at higher temperatures. However, Isotope atoms are chemically identical to the majority of atoms in the material and thus behave similarly in terms of interacting with electrons."""},
-        {"condition": {"output_format": "Coursera Graded Quiz"}, "prompt": """All questions should align with the structure below. Indicate the correct answer by using an asterisk.
-
-What is the threshold diameter below which the electrical conduction of a metal nanowire can become worse than that of the bulk?
-
-A: The atomic distance of the metal.
-Feedback: To learn more about the relationship between the diameter of metal nanowires and their electrical conduction properties, review “Resource Placeholder.”
-
-*B: The electron mean free path in the metal.
-Feedback: Correct, the electron mean free path in the metal is the threshold diameter.
-
-C: The electron de Broglie wavelength in the metal.
-Feedback: To learn more about the relationship between the diameter of metal nanowires and their electrical conduction properties, review “Resource Placeholder.”
-
-D: The mean impurity distance in the metal.
-Feedback: To learn more about the relationship between the diameter of metal nanowires and their electrical conduction properties, review “Resource Placeholder.”"""},
-        {"condition": {"output_format": "H5P Textual Upload Feature"}, "prompt": """Align the format of the questions and answers with the example below. (Each answer option should be on a separate line.) There is no feedback here. 
-
-Example:
-Who founded the Roman city of Barcino, which later became Barcelona?
-
-The Greeks
-*The Romans:::Barcelona was originally founded as a Roman colony named Barcino around the end of the 1st century BC.
-The Visigoths
-The Carthaginians"""},
-        {"condition": {"output_format": "Open edX OLX Quiz"}, "prompt": """Align the quiz question format with the format below:
-
->>Add the question text, or prompt, here. This text is required||You can add an optional tip or note related to the prompt like this. <<
-( ) an incorrect answer {{You can specify optional feedback like this, which appears after this answer is submitted.}}
-(x) the correct answer
-( ) an incorrect answer {{You can specify optional feedback for none, a subset, or all of the answers.}}
-||You can add an optional hint like this. Problems that have a hint include a hint button, and this text appears the first time learners select the button.||
-||If you add more than one hint, a different hint appears each time learners select the hint button.||"""},
+        {"condition": {"output_format": "General Quiz Feedback"}, "prompt": "Please align the output format according to the example provided for the selection General Quiz Feedback."},
+        {"condition": {"output_format": "Answer-Option Level Quiz Feedback"}, "prompt": "Please align the output format according to the example provided for the selection Answer-Option Level Quiz Feedback."},
+        {"condition": {"output_format": "Coursera Ungraded Quiz"}, "prompt": "Please align the output format according to the example provided for the selection Coursera Ungraded Quiz Feedback."},
+        {"condition": {"output_format": "Coursera Graded Quiz"}, "prompt": "Please align the output format according to the example provided for the selection Coursera Graded Quiz."},
+        {"condition": {"output_format": "H5P Textual Upload Feature"}, "prompt": "Please align the output format according to the example provided for the selection H5P Textual Upload Feature."},
+        {"condition": {"output_format": "Open edX OLX Quiz"}, "prompt": "Please align the output format according to the example provided for the selection Open edX OLX Quiz."},
     ]
 
 # Define phases and fields
@@ -155,14 +177,14 @@ PHASES = {
                 "type": "slider",
                 "label": "Number of correct answers per question:",
                 "min_value": 1,
-                "max_value": 6,
+                "max_value": 4,
                 "value": 1
             },
             "distractors_num": {
                 "type": "slider",
                 "label": "Number of distractors per question:",
                 "min_value": 1,
-                "max_value": 4,
+                "max_value": 3,
                 "value": 1
             },
             "text_input": {
@@ -180,11 +202,11 @@ PHASES = {
         "user_prompt": [
             {
                 "condition": {},
-                "prompt": "Please write {questions_num} for {question_level} level multiple-choice question(s), each with {correct_ans_num} correct answer(s) and with {distractors_num} incorrect answers, based on the following text:\n{text_input}\n for {output_format}"
+                "prompt": "Please write {questions_num} multiple-choice question(s) for {question_level} level, each with {correct_ans_num} correct answer(s) and with {distractors_num} incorrect answers, based on the following text:\n{text_input}\n for {output_format}"
             },
             {
                 "condition": {"output_format": True},
-                "prompt": "Align the questions with the {output_format} formatting. Example:\n{example_text}"
+                "prompt": "Align the questions with the {output_format} formatting.}"
             }
         ],
         "ai_response": True,
